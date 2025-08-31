7748ceadff031015f1f9005a8b80096f841caff9
coderBhav <bhch0204@gmail.com>
2025-08-31 17:39:36 +0530

    updating

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,29 +1,42 @@
 import RestaurantCard from "./RestaurantCard";
 import {resList} from "../utils/mockData";
 import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 const Body=()=>{
-    const[topResList,settopResList]=useState(resList);
+    // local state-variable
+    const [topResList,settopResList]=useState([]);
+    const [Copy,setCopy]=useState([]);
+    const [Type,setType]=useState("");
     useEffect(()=>{
         getData();
     },[]);
     const getData=async()=>{
         const data=await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.99740&lng=79.00110&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
         const getJson=await data.json();
-        settopResList(getJson?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-        
+        settopResList(getJson?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants); 
+        setCopy(getJson?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants); 
     };
-    return(
+    return topResList.length==0 ? (
+        <Shimmer/>
+    ):(
         <div className="container">
             <div className="search">
-                <input className="text" type="text" placeholder="Search for restaurant, cuisines, Food"/>
-                <button className="btn" onClick={()=>{
+                <input className="text" type="text" placeholder="Search for restaurant, cuisines, Food" value={Type} onChange={(e)=>{
+                    setType(e.target.value);
+                }}/>
+                <button onClick={()=>{
+                    const resFilter=topResList.filter((res)=>res.info.name.toLowerCase().includes(Type.toLowerCase()));
+                    setCopy(resFilter);
+                }
+                }>Search</button>
+            </div>
+            <button className="btn" onClick={()=>{
                     const topRes=topResList.filter((res)=>res.info.avgRating>4);
                     settopResList(topRes);
                 }}>Top Restaurants</button>
-            </div>
             <div className="res-container">
                 {
-                    topResList.map((restaurant)=>(
+                    Copy.map((restaurant)=>(
                         <RestaurantCard key={restaurant.info.id} resData={restaurant}/>
                     ))
                 }
---
+++ src/components/Shimmer.js
@@ -0,0 +1,12 @@
+const Shimmer=()=>{
+    return(
+        <div className="cards-container">
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+        </div>
+    )
+};
+export default Shimmer;
